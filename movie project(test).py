9290d0fbd8c277febadf8ace88f9ca35e34a04e2
Oliver Wells <oliverwells27@yahoo.com>
2020-05-12 09:14:10 +0200

    Initial commit

---
+++ movie project(test).py
@@ -0,0 +1,44 @@
+## For Users ##
+#user_id = input("Enter your Membership Name: ")
+#print("Hello! " + user_id)
+
+def test():
+    name = input('enter name of the actor:')
+
+    if(name== 'Will Smith') or (name== "will smith"):
+
+        print ("Spies in Disguise")
+        print("I am Legend")
+        print("Gemini Man")
+        print("Suicide Squad")
+
+    else:
+        print("movie unavailable")
+
+test()
+
+def test():
+    name = input('Enter Movie from List:')
+
+    if(name == 'I am Legend') or (name== 'I am legend'):
+        print("post-apocalyptic thriller film")
+        print("This Movie has a 3.5 out of 5 stars")
+
+    elif(name == 'Gemini Man') or (name== 'gemini man'):
+        print("American action thriller film")
+        print ("This Movie has 3 out of 5 stars")
+
+    elif(name == 'Suicide Squad') or (name== 'suicide squad'):
+        print("American Superhero Film")
+        print("This Movie has a 2.5 out of 5 stars")
+
+    elif(name == 'Spies in Disguise') or (name== 'Spies in Disguise'):
+        print("Computer-Animated Spy Comedy Film")
+        print("This Movie has a 4.0 out of 5 stars")
+
+    else:
+        ("This Movie is not available")
+
+test()
+
+
